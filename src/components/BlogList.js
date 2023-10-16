f82e3bc9532503312b212e53eb66a6f005e4c6bc
ALowtonQA <alowton@qa.com>
2023-10-16 16:24:48 +0100

    AL: card styling

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,13 +1,14 @@
+import Card from "./Card";
 
 function BlogList(props) {
     const blogList = props.blogs.map(blog => {
         return (
-            <div key={blog.id}>
+            <Card key={blog.id}>
                 <div>Title:{blog.title}</div>
                 <div>Author: {blog.author}</div>
                 <div>Body: {blog.body}</div>
                 <button onClick={() => {props.deleteBlog(blog.id)}}> Delete </button>
-            </div>
+            </Card>
         )
     });
     return blogList;
---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import "../css/card.css";
+
+function Card(props) {
+    return (
+        <div className="card"> {props.children} </div>
+    )
+}
+
+export default Card;
