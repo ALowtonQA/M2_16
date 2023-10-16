4839b45a9751a8612cc5f5ec4a9c2ede1d032a3f
ALowtonQA <alowton@qa.com>
2023-10-16 16:00:46 +0100

    AL: use state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import BlogList from "./components/BlogList";
 import allBlogs from "./json/blogData.json";
+import { useState } from "react";
 
 function App() {
+
+  const [blogs, setBlogs] = useState(allBlogs);
+
   function deleteBlog(id) {
-    allBlogs = allBlogs.filter((blog) => { return blog.id != id });
-    // allBlogs = allBlogs.filter(blog => blog.id != id); // same as above, but shorter.
+    let filteredBlogs = blogs.filter((blog) => { return blog.id != id });
+    // let filteredBlogs = blogs.filter(blog => blog.id != id); // same as above, but shorter.
+
+    setBlogs(filteredBlogs); // overwrite our state and trigger a rerender with the NEW data
   }
   
   return(
-    <BlogList blogs={allBlogs} deleteBlog={deleteBlog}/>
+    <BlogList blogs={blogs} deleteBlog={deleteBlog}/>
   )
 }
 
