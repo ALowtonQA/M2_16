0684f2fa57d351bdff78236b38a2a296b09b88e1
ALowtonQA <alowton@qa.com>
2023-10-17 12:14:13 +0100

    AL: fetch fixed no infinite loop

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,16 +1,18 @@
 import BlogList from "./BlogList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Blogs() {
 
     const [blogs, setBlogs] = useState(null); // state variable to hold blog data
 
-    fetch("http://localhost:4000/blogs")
-        .then((res) => {
-            return res.json();
-        }).then((data) => {
-            setBlogs(data);
-        });
+    useEffect(() => {
+        fetch("http://localhost:4000/blogs")
+            .then((res) => {
+                return res.json();
+            }).then((data) => {
+                setBlogs(data);
+            });
+    }, []);
 
     function deleteBlog(id) {
       let filteredBlogs = blogs.filter((blog) => { return blog.id != id });
