d6517547560423a8b72c3b747783be8f7c4c4743
ALowtonQA <alowton@qa.com>
2023-10-17 14:33:25 +0100

    AL: added error handling

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -6,17 +6,27 @@ function Blogs() {
 
     const [blogs, setBlogs] = useState(null); // state variable to hold blog data
     const [isLoading, setIsLoading] = useState(true); 
+    const [error, setError] = useState(null); 
 
     useEffect(() => {
         fetch("http://localhost:4000/blogs")
             .then((res) => {
+
+                if (!res.ok) {
+                    throw new Error("Could not get data for that resource.")
+                }
+
                 return res.json();
             }).then((data) => {
                 setTimeout(() => {
                     setIsLoading(false);
+                    setError(null);
                     setBlogs(data);
                 }, 2000);
-            });
+            }).catch((err) => {
+                setIsLoading(false);
+                setError(err.message);
+            })
     }, []); // Empty dependency array so the code only runs once
 
     function deleteBlog(id) {
@@ -28,6 +38,7 @@ function Blogs() {
 
     return (
         <>
+            {error && <div> {error} </div>}
             {blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog}/>}
             {isLoading && <Spinner />}
         </>
